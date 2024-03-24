fd1a37b11cedaa4195c114eea2e815d87d031db8
Nguyen Ba Chuc <nbchuc@hotmail.com>
2024-03-24 12:33:10 +0700

    Create Project

---
+++ src/main/java/com/poly/poly_mobile/PolyMobileApplication.java
@@ -0,0 +1,13 @@
+package com.poly.poly_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/poly_mobile/PolyMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.poly_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
